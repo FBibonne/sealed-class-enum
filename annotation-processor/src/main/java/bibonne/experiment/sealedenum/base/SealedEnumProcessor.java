3bba626d1f205e704eed536c48aca3d53210a7e3
Fabrice Bibonne <fabrice.bibonne@gmail.com>
2025-03-06 07:29:14 +0100

    feat (add getType() to generated enum)

--- annotation-processor/src/main/java/bibonne/experiment/sealedenum/base/SealedEnumProcessor.java
+++ annotation-processor/src/main/java/bibonne/experiment/sealedenum/base/SealedEnumProcessor.java
@@ -17,13 +17,13 @@ import java.util.stream.Stream;
 
 @SupportedAnnotationTypes("bibonne.experiment.sealedenum.base.SealedEnum")
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
-public class SealedEnumProcessor extends AbstractProcessor{
+public class SealedEnumProcessor extends AbstractProcessor {
 
     private Set<? extends TypeElement> targetTypeElement;
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        if (targetTypeElement.equals(annotations)){
+        if (targetTypeElement.equals(annotations)) {
             doProcess(roundEnv);
             return true;
         }
@@ -37,7 +37,7 @@ public class SealedEnumProcessor extends AbstractProcessor{
     private void processElement(Element element) {
         if (element instanceof TypeElement typeElement) {
             var allSubTypes = findAllSubTypes(typeElement);
-            if (! allSubTypes.isEmpty()){
+            if (!allSubTypes.isEmpty()) {
                 createEnumFor(typeElement, allSubTypes);
             }
         }
@@ -54,27 +54,51 @@ public class SealedEnumProcessor extends AbstractProcessor{
         TypeElementVisitor typeElementVisitor = new TypeElementVisitor();
         Filer filer = processingEnv.getFiler();
         ElementName elementName = ElementName.of(typeElement);
-        String simpleEnumName = elementName.simpleName()+"Enum";
-        try (var writer=new PrintWriter(filer.createSourceFile(fullyQualifiedName(elementName.packageName(), simpleEnumName)).openWriter());) {
+        String simpleEnumName = elementName.simpleName() + "Enum";
+        try (var writer = new PrintWriter(filer.createSourceFile(fullyQualifiedName(elementName.packageName(), simpleEnumName)).openWriter());) {
             printPackage(elementName.packageName(), writer);
             printEnumDeclaration(simpleEnumName, writer);
             printEnumConstants(toSimpleNames(permitedTypes, typeElementVisitor), writer);
+            printEnumBody(elementName.simpleName(), simpleEnumName, writer);
             printCloseEnum(writer);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private void printEnumBody(String simpleElementName, String simpleEnumName, PrintWriter writer) {
+        /*
+          Class<? extends SealedClass<?>> clazz;
+          
+          SealedClassEnum(Class<? extends SealedClass<?>> clazz){
+              this.clazz = clazz;
+          }
+          
+          public Class<? extends SealedClass<?>> getType(){
+              return clazz;
+          }
+        */
+        writer.println("""
+                Class<? extends %s> clazz;
+                %s(Class<? extends %s> clazz){
+                    this.clazz = clazz;
+                }
+                public Class<? extends %s> getType(){
+                    return clazz;
+                }
+                """.formatted(simpleElementName, simpleEnumName, simpleElementName, simpleElementName));
+    }
+
     private void printEnumConstants(Stream<String> simpleNames, PrintWriter writer) {
-        simpleNames.map(name->name+", ").forEach(writer::print);
-        writer.println();
+        writer.print(simpleNames.collect(Collectors.joining(",")));
+        writer.println(";");
     }
 
     private Stream<String> toSimpleNames(List<? extends TypeMirror> permitedTypes, TypeElementVisitor typeElementVisitor) {
         return permitedTypes.stream()
                 .map(typeMirror -> typeMirror.accept(typeElementVisitor, null))
-                .map(TypeElement::getSimpleName)
-                .map(Objects::toString);
+                .map(typeElement -> typeElement.getSimpleName() + "(" + typeElement.getQualifiedName() + ".class)");
+        //ex:  StringDecoder(ParameterValueDecoder.StringDecoder.class)
     }
 
     private void printCloseEnum(PrintWriter writer) {
@@ -82,11 +106,11 @@ public class SealedEnumProcessor extends AbstractProcessor{
     }
 
     private void printEnumDeclaration(String enumName, PrintWriter writer) {
-        writer.println("public enum "+enumName+" {");
+        writer.println("public enum " + enumName + " {");
     }
 
     private void printPackage(Optional<String> packageName, PrintWriter writer) {
-        packageName.ifPresent(name->writer.println("package "+name+";"));
+        packageName.ifPresent(name -> writer.println("package " + name + ";"));
     }
 
     private CharSequence fullyQualifiedName(Optional<String> packageName, String simpleEnumName) {
@@ -107,10 +131,10 @@ public class SealedEnumProcessor extends AbstractProcessor{
         public static ElementName of(TypeElement typeElement) {
             String typeElementFullyQualifiedName = typeElement.getQualifiedName().toString();
             Matcher matcher = namePattern.matcher(typeElementFullyQualifiedName);
-            if (matcher.matches()){
+            if (matcher.matches()) {
                 // the regular exepression matches so there is a non empty package :
                 return new ElementName(Optional.of(matcher.group(1)), matcher.group(2));
-            }else{
+            } else {
                 return new ElementName(Optional.empty(), typeElementFullyQualifiedName);
             }
         }
